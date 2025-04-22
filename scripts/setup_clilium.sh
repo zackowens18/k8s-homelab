0098bb8f9c6336044d90effc6cab739a5bc7d798
Zack Owens <zowens189@gmail.com>
2025-04-22 16:42:59 -0400

    Fixing issues around wrong path and adding in test cluster. Adding in some of the k8s basic helm charts

---
+++ scripts/setup_clilium.sh
@@ -0,0 +1,2 @@
+# TO DO install helm
+helm install cilium cilium/cilium --version 1.15.6 --namespace kube-system --set ipam.mode=kubernetes --set kubeProxyReplacement=false --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" --set cgroup.autoMount.enabled=false --set cgroup.hostRoot=/sys/fs/cgroup
